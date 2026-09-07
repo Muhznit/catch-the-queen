189ac2c05bf94d6fef5403ef7d3d2bff8fcd1229
Muhznit <muhznit@gmail.com>
2026-09-07 17:37:50 -0500

    One of many WIP versions of main_gui.py

--- main_gui.py
+++ main_gui.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+import dataclasses
+import math
+import random
+
 import pygame
 import pygame_gui
 
@@ -45,10 +49,147 @@ class EngineState:
 # TODO: For collision detection maybe make use of
 # the second code snippet at https://www.pygame.org/wiki/QuadTree
 
+@dataclasses.dataclass(slots=True)
+class BallSpawner:
+    x: int
+    y: int
+    radius: float
+
+    @property
+    def pos(self):
+        return pygame.math.Vector2(self.x, self.y)
+
+
+@dataclasses.dataclass(slots=True)
+class Ball:
+    pos: pygame.math.Vector2 = dataclasses.field(default_factory=pygame.math.Vector2)
+    vel: pygame.math.Vector2 = dataclasses.field(default_factory=pygame.math.Vector2)
+    acc: pygame.math.Vector2 = dataclasses.field(default_factory=pygame.math.Vector2)
+
+
+def update_basic_physics(dt, obj):
+    obj.vel += obj.acc
+    obj.pos += obj.vel
+    obj.acc *= 0
+    obj.vel *= .9
+
+
+def update_bounce_off_screen(dt, obj):
+    future_pos = obj.pos + obj.vel + obj.acc
+    if future_pos.x > WINDOW_WIDTH:
+        old_x = obj.pos.x
+        obj.pos.x = WINDOW_WIDTH
+        obj.vel.x -= obj.pos.x - old_x
+        obj.vel.x *= -1
+
+    if future_pos.x < 0:
+        old_x = obj.pos.x
+        obj.pos.x = 0
+        obj.vel.x -= obj.pos.x - old_x
+        obj.vel.x *= -1
+
+
+def update_wrap_around_screen(dt, obj):
+    obj.pos.x = (WINDOW_WIDTH + obj.pos.x) % WINDOW_WIDTH
+    obj.pos.y = (WINDOW_HEIGHT + obj.pos.y) % WINDOW_HEIGHT
+
+
+def calc_cohesion_vectors(boids):
+    midpoint = pygame.math.Vector2(0, 0)
+    for boid in boids:
+        midpoint += boid.pos
+
+    midpoint /= len(boids)
+
+    for boid in boids:
+        direction = midpoint - boid.pos
+        direction.normalize_ip()
+        yield direction
+
+
+def calc_alignment_vectors(boids):
+    heading = pygame.math.Vector2(0, 0)
+    for boid in boids:
+        heading += boid.vel
+    heading.normalize_ip()
+
+    for boid in boids:
+        yield heading
+
+
+def calc_separation_vectors(boids):
+    min_distance = 8
+    for boid in boids:
+        heading = pygame.math.Vector2(0, 0)
+        for other_boid in boids:
+            distance = boid.pos.distance_to(other_boid.pos)
+
+            pos_diff = boid.pos - other_boid.pos
+            if pos_diff.length() < min_distance:
+                heading += pos_diff
+        yield heading
+
+
+def render_ball_spawner(surface, color, instance):
+    pygame.draw.circle(surface, color, (instance.x, instance.y), instance.radius)
+
+
+def render_crosshair(surface, color, center, radius):
+    pygame.draw.line(
+        surface, color,
+        (center.x, center.y - radius),
+        (center.x, center.y + radius)
+    )
+    pygame.draw.line(
+        surface, color,
+        (center.x - radius, center.y),
+        (center.x + radius, center.y)
+    )
+
+
+# Application States: States that the whole app can be in.
 class StartState(EngineState):
+    def __init__(self):
+        self.home_zone = BallSpawner(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, 64)
+        self.cursor = self.home_zone.pos
+        self.balls = list()
+        for _ in range(16):
+            ball = Ball(self.home_zone.pos)
+            ball.acc.x = random.randint(1, 8) * random.choice([-1, 1])
+            ball.acc.y = random.randint(1, 8) * random.choice([-1, 1])
+            ball.pos.x = ball.pos.x + math.cos(_ / 16 * 2 * math.pi) * 64
+            ball.pos.y = ball.pos.y + math.sin(_ / 16 * 2 * math.pi) * 64
+            self.balls.append(ball)
+
+    def update(self, dt):
+        x, y = get_user_input_tuple()
+
+        self.cursor.update(pygame.mouse.get_pos())
+
+        for ball in self.balls:
+            update_basic_physics(dt, ball)
+            #update_bounce_off_screen(dt, ball)
+            update_wrap_around_screen(dt, ball)
+
+        cohesions = tuple(calc_cohesion_vectors(self.balls))
+        separations = tuple(calc_separation_vectors(self.balls))
+        alignments = tuple(calc_alignment_vectors(self.balls))
+        for i, ball in enumerate(self.balls):
+            ball.acc += cohesions[i] + separations[i] + alignments[i]
+            if ball.acc.length() > 4:
+                ball.acc.scale_to_length(4)
+
     def render(self, surface):
-        font = pygame.font.SysFont("Courier New", 32)
         surface.fill(pygame.Color("#505050"))
+
+        render_ball_spawner(surface, "#FF0000", self.home_zone)
+
+        radius = 8.0
+        render_crosshair(surface, "#FFFFFF", self.cursor, radius)
+
+        for ball in self.balls:
+            pygame.draw.circle(surface, "#800000", ball.pos, radius)
+        font = pygame.font.SysFont("Courier New", 32)
         font_surf = font.render(
             "starting state", False,
             (255, 255, 255),
